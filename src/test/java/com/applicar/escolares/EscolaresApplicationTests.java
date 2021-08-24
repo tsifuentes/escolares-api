cad8a6e5c22c5754e68624872c0d4428b4c28295
Thomas Sifuentes <smthomas1203@gmail.com>
2021-08-24 14:42:17 -0500

    feat: setup project

---
+++ src/test/java/com/applicar/escolares/EscolaresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applicar.escolares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
